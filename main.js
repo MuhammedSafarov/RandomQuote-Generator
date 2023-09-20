f8570db161ef5d3dfd9576c54a5acc5138642e53
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-09-20 11:08:53 +0400

    Design is finish

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-let quoteFetch = async () => {
-  let res = await fetch("https://type.fit/api/quotes")
-    .then((res) => res.json())
-    .then((data) => {
-      var randomQuote = data[Math.floor(Math.random() * data.length)];
-      console.log(randomQuote);
-    });
-};
 
-quoteFetch();
+
