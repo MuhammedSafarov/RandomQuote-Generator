3e3d6ac163ac28d398adfc5f95546fa15b98a674
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-09-20 12:17:41 +0400

    Loader added to App

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var generateBtn = document.querySelector("#generate-btn");
 var quote = document.querySelector(".quote");
 var author = document.querySelector(".author");
+var loader = document.querySelector(".loader");
 
 let quoteFetch = async () => {
   quote.innerHTML = "";
   author.innerHTML = "";
+
+  if (quote.innerHTML == "" && author.innerHTML == "") {
+    loader.style.display = "inline-block";
+  }
+
   let res = await fetch("https://type.fit/api/quotes")
     .then((res) => res.json())
     .then((data) => {
+      loader.style.display = "none";
       let randomQuote = data[Math.floor(Math.random() * data.length)];
       quote.innerHTML += randomQuote.text;
       author.innerHTML += "~ " + randomQuote.author;
