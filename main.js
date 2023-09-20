81c730ee1de2680454dfa6de883941c8e1b4245b
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-09-20 11:19:29 +0400

    Quote and author fetched and showed in app

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let generateBtn = document.querySelector("#generate-btn");
+var generateBtn = document.querySelector("#generate-btn");
+var quote = document.querySelector(".quote");
+var author = document.querySelector(".author");
 
 let quoteFetch = async () => {
+  quote.innerHTML = "";
+  author.innerHTML = "";
   let res = await fetch("https://type.fit/api/quotes")
     .then((res) => res.json())
     .then((data) => {
-      var randomQuote = data[Math.floor(Math.random() * data.length)];
-      console.log(randomQuote);
+      let randomQuote = data[Math.floor(Math.random() * data.length)];
+      quote.innerHTML += randomQuote.text;
+      author.innerHTML += randomQuote.author;
     });
 };
 
 generateBtn.addEventListener("click", quoteFetch);
-
