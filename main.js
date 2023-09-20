b1d02e918984da870083dedb5e268b761164507a
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-09-20 11:12:05 +0400

    data can successfully fetched

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+let generateBtn = document.querySelector("#generate-btn");
 
+let quoteFetch = async () => {
+  let res = await fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      var randomQuote = data[Math.floor(Math.random() * data.length)];
+      console.log(randomQuote);
+    });
+};
+
+generateBtn.addEventListener("click", quoteFetch);
 
