695a257dd49ba9f5be754296cbe585ed384233a1
Muxammed Safarov <muxammed@Muxammeds-MacBook-Pro.local>
2023-09-22 12:38:15 +0400

    Optimized

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 let generateBtn = document.querySelector("#generate-btn");
 let quote = document.querySelector(".quote");
 let author = document.querySelector(".author");
-let loader = document.querySelector(".loader");
+const url = "https://type.fit/api/quotes";
+let storage = null;
 
 let quoteFetch = async () => {
-  quote.innerHTML = "";
-  author.innerHTML = "";
-
   if (quote.innerHTML == "" && author.innerHTML == "") {
-    loader.style.display = "inline-block";
   }
-
-  let res = await fetch("https://type.fit/api/quotes")
+  let res = await fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      loader.style.display = "none";
-      let randomQuote = data[Math.floor(Math.random() * data.length)];
-      quote.innerHTML += randomQuote.text;
-      author.innerHTML += "~ " + randomQuote.author;
+      localStorage.setItem("quotes", JSON.stringify(data));
     });
 };
+quoteFetch();
 
-generateBtn.addEventListener("click", quoteFetch);
+storage = JSON.parse(localStorage.getItem("quotes"));
+
+let eventHandler = () => {
+  quote.innerHTML = "";
+  author.innerHTML = "";
+  var randomQuote = storage[Math.floor(Math.random() * storage.length)];
+  quote.innerHTML += randomQuote.text;
+  author.innerHTML += "~ " + randomQuote.author;
+};
+
+generateBtn.addEventListener("click", eventHandler);
